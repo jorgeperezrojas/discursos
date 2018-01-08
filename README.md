396c637b10edc84c5b120b1cc1075b0b7dfd6ea8
Jorge Pérez <jorge.perez.rojas@gmail.com>
2018-01-08 00:35:37 -0300

    opciones para frecuencias de palabras dentro de párrafos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Discursos presidenciales
+# Discursos presidenciales chilenos
 
-Datos originales de discursos presidenciales obtenidos desde la Web, y scripts para procesarlos (extraer texto, limpiarlo y eliminar duplicados). Actualmente contiene:
+Datos originales de discursos presidenciales chilenos obtenidos desde la Web, y scripts para procesarlos (extraer texto, limpiarlo y eliminar duplicados). Actualmente contiene:
 
 - 575 discursos de Sebastián Piñera desde 2010 hasta 2014, obtenidos desde el sitio http://2010-2014.gob.cl/discursos/ (descargados en diciembre 2017). Todos los discursos se encuentran en archivos en formato .htm. Aproximadamente 563 son discursos únicos.
 - 1581 discursos de Michelle Bachelet desde 2014 hasta 2017, obtenidos desde el sitio https://prensa.presidencia.cl/discursos.aspx (descargados en diciembre 2017). Todos los discursos se encuentran en archivos en formato .html. Aproximadamente 1539 son discursos únicos.
@@ -24,11 +24,11 @@ python src/htm_to_txt_plus_meta_piñera.py
 
 en la raiz (requiere python 3, un par de librerías estandar y [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/)). Esto también almacena archivos (meta.txt) con algunos metadatos de cada discurso obtenidos desde los .htm y .html correspondientes (como fecha, títulos, subtítulos y direcciones de archivos de imágenes para ciertos discursos).
 
-Para limpiar los datos de texto, eliminando textos con muy pocos caracteres útiles y eliminar algunos discursos duplicados (o discursos que son realmente extractos de otros), se debe ejecutar
+Para limpiar los datos de texto, eliminando textos con muy pocos caracteres útiles, eliminar algunos discursos duplicados (o discursos que son realmente extractos de otros), y eliminar párrafos con palabras de baja frecuencia (para borrar partes en idioma no español, por ejemplo) se debe ejecutar
 
 ```
-python src/text_processing.py data/bachelet_2/ --duplicateDeletion --verbose
-python src/text_processing.py data/piñera_1/ --duplicateDeletion --verbose
+python src/text_processing.py data/bachelet_2/ --duplicateDeletion --frequencyCleaning 0.19 --verbose
+python src/text_processing.py data/piñera_1/ --duplicateDeletion --frequencyCleaning 0.19 --verbose
 ```
 
 El script tiene varias otras opciones para regular la limpieza de los textos.
--- src/text_processing.py
+++ src/text_processing.py
@@ -3,7 +3,9 @@
 import argparse
 import re
 import os
+import shutil
 from check_duplicates import check_duplicates
+from collections import Counter
 
 # letras y números en expresiones regulares
 re_nums = '0-9'
@@ -11,6 +13,7 @@ re_letters = 'a-záéíóúüA-ZÁÉÍÓÚÜñÑ'
 re_punctuation_and_space = r'\(\)¿\?!¡\,;.:&\$% '
 re_skip_chars = '[^' + re_nums + re_letters + re_punctuation_and_space + ']' 
 re_skip_strings = re_skip_chars + '+'
+re_chars_for_splitting = '[' + re_punctuation_and_space + '–"\n' + ']+'
 re_months = r'(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)'
 re_date_spec = '(([' + re_letters + ']{1,30} ?){1,3}\,? +)?[0-9]{1,2}( +de)? +' + re_months + '( +del?)? +2[0-9]{3}\.{0,10}$'
 re_html_garbage = r'((br| ) )*((&([a-z0-9]+;)+(br)*)+)'
@@ -28,6 +31,7 @@ def is_date(line):
 
 def clean_text(raw_text):
     text = raw_text.strip()
+    text = raw_text.replace(u'\xa0', u' ')
     for exp in expressions_to_delete:
         if re.search(exp,text):
             text = re.sub(exp,'',text)
@@ -35,7 +39,7 @@ def clean_text(raw_text):
     text = re.sub(' +',' ',text)
     return text
 
-def is_useful_line(line,only_spanish=False):
+def is_useful_line(line,word_counts=None,frequency=0.19,min_len=50):
     if line == '':
         return False
 
@@ -45,6 +49,23 @@ def is_useful_line(line,only_spanish=False):
 
     if is_date(line): # the line describes the date of the discurso
         return False
+
+    if len(line) < min_len: # too short to be useful
+        return False
+
+    if frequency > 0:
+        if word_counts == None:
+            raise Exception('No word count provided')
+
+        voc_size = len(word_counts)
+        line_avg = 0
+        words = re.split(re_chars_for_splitting,line.strip())
+        for word in words:
+            line_avg += word_counts[word]
+        line_avg = line_avg / len(words) / len(word_counts)
+        if line_avg < frequency:
+            return False
+
 
     for banned_prefix in banned_prefixes:
         if line.lower().startswith(banned_prefix.lower()):
@@ -58,8 +79,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('base_directory', metavar='D', type=str,
                         help='Base directory')
-    parser.add_argument('-s', '--onlySpanish', default=False, action='store_true',
-                        help='Try to keep only spanish text.')
+    parser.add_argument('-fC', '--frequencyCleaning', metavar='F', type=float, default=0.19,
+                        help='Discard paragraphs with average frequency of words below F (this is to keep out non Spanish discursos).')
     parser.add_argument('-v', '--verbose', default=False, action='store_true',
                         help='Show in detail what the script is doing.')
     parser.add_argument('-mC', '--minUsefulChars', metavar='MC', type=int, default=1000, 
@@ -75,22 +96,53 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     base_directory = args.base_directory
-    only_spanish = args.onlySpanish
+    frequency = args.frequencyCleaning
     duplicate_deletion = args.duplicateDeletion
     full = args.fullProcessing
     min_useful_chars = args.minUsefulChars
     look_ahead = args.lookAhead
     n_equals = args.numEquals
     verbose = args.verbose
+    word_counts = None
 
     txt_directory = os.path.join(base_directory, 'raw_text/')
     processed_directory = os.path.join(base_directory, 'processed_text/')
     clean_d_directory = os.path.join(processed_directory,'discursos_limpios')
 
+
+    shutil.rmtree(clean_d_directory)
+    shutil.rmtree(processed_directory)
+
     os.makedirs(processed_directory)
     os.makedirs(clean_d_directory)
 
+    if frequency > 0:
+        ### for now we are computing internal word frequencies
+        ### it would be better to rely in some external word frequency corpus
+        if verbose:
+            print('Computing word frequency statistics for cleaning...')
+        # genera una tabla de frecuencia de palabras
+        all_lines = []
+        all_words = []
+
+        for filename in os.listdir(txt_directory):
+            if filename == 'meta.txt':
+                continue
+            raw_text = open(os.path.join(txt_directory,filename)).read()
+            raw_lines = raw_text.split('\n')
+            clean_lines = [clean_text(l) for l in raw_lines]
+            list_of_lists_of_words = [re.split(re_chars_for_splitting,line.strip()) for line in clean_lines]
+            for list_of_words in list_of_lists_of_words:
+                all_lines.append(list_of_words)
+                all_words += list_of_words
+
+        word_counts = Counter(all_words)
+        # compute average frequency per paragraph
+
+
     # ahora limpia el texto y guarda uno a uno en archivos distintos
+    if verbose:
+        print('Begin cleaning...')
     for filename in os.listdir(txt_directory):
         if filename == 'meta.txt':
             continue
@@ -102,7 +154,7 @@ if __name__ == '__main__':
 
         for line in raw_lines:
             clean_line = clean_text(line)
-            if is_useful_line(clean_line, only_spanish=only_spanish):
+            if is_useful_line(clean_line, word_counts=word_counts, frequency=frequency):
                 useful_lines.append(clean_line)
 
         paragraphs = '\n'.join(useful_lines)
@@ -125,7 +177,6 @@ if __name__ == '__main__':
 
 
     if full:
-
         # primero junta todos los nombres y ordénalos
         filenames = []
         for filename in os.listdir(txt_directory):
